b5f6be821ae7a63c7aa98fcc1ed156bb0fb495fa
DemianczukM <DemianczukM@K273S75J>
2019-03-05 09:53:32 +0100

    struct Otwor

--- KoordynatyOtworow/Otwor.cs
+++ KoordynatyOtworow/Otwor.cs
@@ -12,5 +12,12 @@ namespace KoordynatyOtworow
         public double PozX;
         public double PozY;
         public double PozZ;
+
+        public Otwor(double fi, double x, double y, double z) {
+            Srednica = Math.Round(fi,1);
+            PozX = Math.Round(x);
+            PozY = Math.Round(y);
+            PozZ = Math.Round(z);
+        }
     }
 }
