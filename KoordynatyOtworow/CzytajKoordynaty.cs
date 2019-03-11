bfaf70c8fe13053a9c28ac1fcd65db55a329d5c8
DemianczukM <DemianczukM@K273S75J>
2019-03-11 09:57:31 +0100

    po staremu

--- KoordynatyOtworow/CzytajKoordynaty.cs
+++ KoordynatyOtworow/CzytajKoordynaty.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
-using Microsoft.Win32;
+//using Microsoft.Win32;
 using AutoCAD;
 
 namespace KoordynatyOtworow { 
